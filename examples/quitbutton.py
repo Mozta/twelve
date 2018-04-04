a85d7afb7a43e3078eb1c79d5c63f09abd32d418
mozta <viper_rafa_@hotmail.com>
2018-04-04 18:03:01 -0500

    scripts qt

---
+++ examples/quitbutton.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt5.QtWidgets import QWidget, QPushButton, QApplication
+
+class Example(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.initUI()
+        
+        
+    def initUI(self):               
+        
+        qbtn = QPushButton('Salir', self)
+        qbtn.clicked.connect(QApplication.instance().quit)
+        qbtn.resize(qbtn.sizeHint())
+        qbtn.move(50, 50)       
+        
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Salir')    
+        self.show()
+        
+        
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
