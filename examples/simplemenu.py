50af9c70686cdc3557ec534a12f516415a55f060
mozta <viper_rafa_@hotmail.com>
2018-04-06 11:28:52 -0500

    Convert ui to py

--- examples/simplemenu.py
+++ examples/simplemenu.py
@@ -7,16 +7,16 @@ from PyQt5.QtGui import QIcon
 
 
 class Example(QMainWindow):
-    
+
     def __init__(self):
         super().__init__()
-        
+
         self.initUI()
-        
-        
-    def initUI(self):               
-        
-        exitAct = QAction(QIcon('exit.png'), '&Exit', self)        
+
+
+    def initUI(self):
+
+        exitAct = QAction('&Exit', self)        
         exitAct.setShortcut('Ctrl+Q')
         exitAct.setStatusTip('Exit application')
         exitAct.triggered.connect(qApp.quit)
@@ -26,14 +26,14 @@ class Example(QMainWindow):
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('&File')
         fileMenu.addAction(exitAct)
-        
+
         self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('Simple menu')    
+        self.setWindowTitle('Simple menu')
         self.show()
-        
-        
+
+
 if __name__ == '__main__':
-    
+
     app = QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
